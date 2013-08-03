1bd2ffe29b516b66342c6f51757231a5f3ad8c9b
chloe0810 <chloechiang.2011@smu.edu.sg>
2013-08-03 17:12:44 +0800

    Update and rename Staff.java to Faculty.java

---
+++ Faculty.java
@@ -0,0 +1,48 @@
+public class Faculty{
+  @Id private Long id;
+  private String facultyEmail;
+  private String facultyName;
+  private String facultyContact;
+  
+  public Faculty(){}
+  
+  public Faculty(Long id, String facultyEmail, String facultyName, String facultyContact){
+    this(facultyEmail, facultyName, facultyContact);
+    this.id=id;
+  }
+  
+  public Faculty(String facultyEmail, String facultyName, String facultyContact){
+    this.facultyEmail = facultyEmail;
+    this.facultyName = facultyName;
+    this.facultyContact = facultyContact;
+  }
+  
+  public Long getId(){
+    return id;
+  }
+  
+  public String getFacultyEmail(){
+    return facultyEmail;
+  }
+  
+  public void setFacultyEmail(String facultyEmail){
+    this.facultyEmail = facultyEmail;
+  }
+  
+  public String getFacultyName(){
+    return facultyName;
+  }
+  
+  public void setFacultyName(String facultyName){
+    this.facultyName = facultyName;
+  }
+  
+  public String getFacultyContact(){
+    return facultyContact;
+  }
+  
+  public void setFacultyContact(String facultyContact){
+    this.facultyContact = facultyContact;
+  }
+
+}
