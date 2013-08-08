831b4068bd7c8efe31b6a55b35ef1ffde2eda4bc
chloe0810 <chloechiang.2011@smu.edu.sg>
2013-08-08 19:16:01 +0800

    Create Role.java

---
+++ Role.java
@@ -0,0 +1,25 @@
+public class Role{
+  
+  @Id private Long id;
+  private String type;
+  
+  public Role(){}
+  
+  public Role(Long id, String type){
+    this(type);
+    this.id = id;
+  }
+  
+  public Role(String type){
+    this.type = type;
+  }
+  
+  public String getType(){
+    return type;
+  }
+  
+  public void setType(String type){
+    this.type = type;
+  }
+  
+}
