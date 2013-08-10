0ac0ef33fb4ccc32356a76ac870cbe971bc44740
chloe0810 <chloechiang.2011@smu.edu.sg>
2013-08-10 11:19:03 +0800

    Create Term.java

---
+++ Term.java
@@ -0,0 +1,27 @@
+public class Term{
+  @Id private Long id;
+  private String name;
+  
+  public Term(){}
+  
+  public Term(Long id, String name){
+    this(name);
+    this.id = id;
+  }
+  
+  public Term(String name){
+    this.name = name;
+  }
+  
+  public Long getId(){
+    return id;
+  }
+  
+  public String getName(){
+    return name;
+  }
+  
+  public void setName(String name){
+    this.name = name;
+  }
+}
