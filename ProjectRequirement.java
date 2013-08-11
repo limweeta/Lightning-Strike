fba0d368d879892fcc4ab4d84c83a43c8524b86a
chloe0810 <chloechiang.2011@smu.edu.sg>
2013-08-11 14:42:40 +0800

    Create ProjectRequirement.java

---
+++ ProjectRequirement.java
@@ -0,0 +1,37 @@
+public class ProjectRequirement{
+  @Id private Long id;
+  private Long projectId;
+  private Long requirementId;
+  
+  public ProjectRequirement(){}
+  
+  public ProjectRequirement(Long id, Long projectId, Long requirementId){
+    this(projectId, requirementId);
+    this.id = id;
+  }
+  
+  public ProjectRequirement(Long projectId, Long requirementId){
+    this.projectId = projectId;
+    this.requirementId = requirementId;
+  }
+  
+  public Long getId(){
+    return id;
+  }
+  
+  public Long getProjectId(){
+    return projectId;
+  }
+  
+  public void setProjectId(Long projectId){
+    this.projectId = projectId;
+  }
+  
+  public Long getRequirementId(){
+    return requirementId;
+  }
+  
+  public void setRequirementId(Long requirementId){
+    this.requirementId = requirementId;
+  }
+}
